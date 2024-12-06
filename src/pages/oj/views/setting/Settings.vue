6dd56bb94c4894a62be65c15ae41ac92049be583
TJU_XuAn <3011725873@qq.com>
2024-12-06 19:02:14 +0800

    No securitysettings ver

--- src/pages/oj/views/setting/Settings.vue
+++ src/pages/oj/views/setting/Settings.vue
@@ -22,7 +22,6 @@
 
             <Menu-item name="/setting/profile">{{$t('m.Profile')}}</Menu-item>
             <Menu-item name="/setting/account">{{$t('m.Account')}}</Menu-item>
-            <Menu-item name="/setting/security">{{$t('m.Security')}}</Menu-item>
           </Menu>
         </div>
         <div class="panel">
